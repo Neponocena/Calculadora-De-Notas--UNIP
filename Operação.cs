fda998b2a3683360be91eca34be0fd70c50fb9b7
Neponocena <pedronepo2005@gmail.com>
2026-01-05 15:12:30 -0300

    feat: :tada: Adiciona lógica para exame final e DP

--- Operação.cs
+++ Operação.cs
@@ -11,19 +11,37 @@ namespace CalculadoraUnip
             Console.WriteLine("------------------------------");
 
             Console.Write("NOTA DA NP1:");
-            float np1 = float.Parse(Console.ReadLine());
-            
+            float np1;
+            if(!float.TryParse(Console.ReadLine(), out np1))
+            {
+                Console.WriteLine("Digite uma nota valída");
+                Console.ReadKey();
+                return;
+            }
             
             Console.WriteLine();
 
             Console.Write("NOTA DA NP2:");
-            float np2 = float.Parse(Console.ReadLine());
+            float np2;
+            if(!float.TryParse(Console.ReadLine(), out np2))
+            {
+                Console.WriteLine("Digite uma nota valída");
+                Console.ReadKey();
+                return;
+            }
+
             
 
             Console.WriteLine("");
 
             Console.Write("NOTA DO PIM:");
-            float pim = float.Parse(Console.ReadLine());
+            float pim;
+            if(!float.TryParse(Console.ReadLine(), out pim))
+            {
+                Console.WriteLine("Digite uma nota valída");
+                Console.ReadKey();
+                return;
+            }
             
 
             float resultado = (np1 * 0.4f) + (np2 * 0.4f) + (pim * 0.2f);
@@ -32,16 +50,37 @@ namespace CalculadoraUnip
             if(resultado >= 7)
             {
                 
-                Console.WriteLine($"Sua media é {resultado}, Você está Aprovado!");
+                Console.WriteLine($"Sua media é {resultado} , Você está Aprovado!");
                 Console.ReadKey();
                 return;
             }
             else
             {
-                Console.WriteLine($"Sua média é {resultado}, Você está de Exame!");
+                Console.WriteLine($"Sua média é {resultado} , Você está de Exame!");
                 Console.ReadKey();
-                return;
+                Console.WriteLine("");
+                Console.WriteLine("Digite a nota do exame");
+                int exame;
+                if(!int.TryParse(Console.ReadLine(), out exame))
+                {
+                    Console.WriteLine("Digite uma nota valída");
+                    Console.ReadKey();
+                    return;
+                }
 
+                if( resultado + exame >= 10)
+                {
+                    Console.WriteLine("Você esta aprovado!");
+                    Console.ReadKey();
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Você está de DP!");
+                    Console.ReadKey();
+                    return;
+                }
+                
             }
 
 
